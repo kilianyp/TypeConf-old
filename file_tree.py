96742618b3e49ebbfa4dcb56f50f77a49074b76d
Kilian Pfeiffer <kilian.pfeiffer@rwth-aachen.de>
2019-11-24 22:44:16 +0100

    Be more explicit about functionality in file tree

--- file_tree.py
+++ file_tree.py
@@ -8,6 +8,7 @@ class FileTree(object):
     def add(self, name, structure):
         """This does not work
         if a directory is added before the file.
+        Adds a file strucuture[-1] with name to the tree
         """
         level = self.nested
         for s in structure[:-1]:
@@ -17,11 +18,24 @@ class FileTree(object):
         level[structure[-1]] = name
 
     def get(self, structure):
+        """
+        Args:
+            structure: The single parts of the path
+
+        Returns:
+            All files 
+        """
         level = self.nested
         for s in structure:
             if s not in level:
                 raise ValueError(f"File {s} does not exists in {level}")
             level = level[s]
-        return level.values()
+        # TODO does not support nested folders
+        all_files = []
+        for val in level.values():
+            if isinstance(val, dict):
+                continue
+            all_files.append(val)
+        return all_files
 
 
